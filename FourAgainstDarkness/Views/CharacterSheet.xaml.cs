9402a023ac1549169358eebfc177f15a199e1dce
bobwegle <rwegle@gmail.com>
2018-07-31 16:23:37 -0500

    final char sheet

--- FourAgainstDarkness/Views/CharacterSheet.xaml.cs
+++ FourAgainstDarkness/Views/CharacterSheet.xaml.cs
@@ -27,26 +27,31 @@ namespace FourAgainstDarkness.Views
 		Character _character1;
 		Character _character2;
 		Character _character3;
+		Character _character4;
 		
 		public CharacterSheet()
 		{
 			Character _char1 = new Character();
 			Character _char2 = new Character();
 			Character _char3 = new Character();
+			Character _char4 = new Character();
 			
 			_char1 = MainClass.GameParty.Characters[0];
 			_char2=MainClass.GameParty.Characters[1];
 			_char3=MainClass.GameParty.Characters[2];
+			_char4=MainClass.GameParty.Characters[3];
 			
 			_character1=_char1;
 			_character2=_char2;
 			_character3=_char3;
+			_character4=_char4;
 			
 			InitializeComponent();
 			
 			this.Panel1.DataContext = _char1;
 			this.Panel2.DataContext = _char2;
 			this.Panel3.DataContext=_char3;
+			this.Panel4.DataContext= _char4;
 							
 			Image img1 = this.Resources[_char1.CharType] as Image;
 			img1.Height=100;
@@ -60,9 +65,14 @@ namespace FourAgainstDarkness.Views
 			img3.Height=100;
 			img3.Width=100;
 			
+			Image img4 = this.Resources[_char4.CharType] as Image;
+			img4.Height=100;
+			img4.Width=100;
+			
 			picpanel1.Children.Add(img1);
 			picpanel2.Children.Add(img2);
 			picpanel3.Children.Add(img3);
+			picpanel4.Children.Add(img4);
 		}
 		
 		void button1_Click(object sender, RoutedEventArgs e)
@@ -83,6 +93,24 @@ namespace FourAgainstDarkness.Views
 			}
 		}
 		
+		void button13_Click(object sender, RoutedEventArgs e)
+		{
+			if(_character3.Currentlife!=0)
+			{
+				_character3.Currentlife=_character3.Currentlife-1;
+				currliferun3.GetBindingExpression(Run.TextProperty).UpdateTarget();
+			}
+		}
+		
+		void button14_Click(object sender, RoutedEventArgs e)
+		{
+			if(_character4.Currentlife!=0)
+			{
+				_character4.Currentlife=_character4.Currentlife-1;
+				currliferun4.GetBindingExpression(Run.TextProperty).UpdateTarget();
+			}
+		}
+		
 		void button2_Click(object sender, RoutedEventArgs e)
 		{
 			if(_character1.Currentlife<_character1.Life)
@@ -98,6 +126,24 @@ namespace FourAgainstDarkness.Views
 			{
 				_character2.Currentlife=_character2.Currentlife+1;				
 				currliferun2.GetBindingExpression(Run.TextProperty).UpdateTarget();			
+			}
+		}
+		
+		void button23_Click(object sender, RoutedEventArgs e)
+		{
+			if(_character3.Currentlife<_character3.Life)
+			{
+				_character3.Currentlife=_character3.Currentlife+1;				
+				currliferun3.GetBindingExpression(Run.TextProperty).UpdateTarget();			
+			}
+		}
+		
+		void button24_Click(object sender, RoutedEventArgs e)
+		{
+			if(_character4.Currentlife<_character4.Life)
+			{
+				_character4.Currentlife=_character4.Currentlife+1;				
+				currliferun4.GetBindingExpression(Run.TextProperty).UpdateTarget();			
 			}
 		}
 		
@@ -121,7 +167,27 @@ namespace FourAgainstDarkness.Views
 			levelbox2.GetBindingExpression(TextBox.TextProperty).UpdateTarget();
 			attackbonusbox2.GetBindingExpression(TextBox.TextProperty).UpdateTarget();
 		}
-				
+			
+		void button33_Click(object sender, RoutedEventArgs e)
+		{
+			_character3.LevelUp();
+			_character3.SetAttackBonus();
+			liferun3.GetBindingExpression(Run.TextProperty).UpdateTarget();
+			currliferun3.GetBindingExpression(Run.TextProperty).UpdateTarget();
+			levelbox3.GetBindingExpression(TextBox.TextProperty).UpdateTarget();
+			attackbonusbox3.GetBindingExpression(TextBox.TextProperty).UpdateTarget();
+		}
+		
+		void button34_Click(object sender, RoutedEventArgs e)
+		{
+			_character4.LevelUp();
+			_character4.SetAttackBonus();
+			liferun4.GetBindingExpression(Run.TextProperty).UpdateTarget();
+			currliferun4.GetBindingExpression(Run.TextProperty).UpdateTarget();
+			levelbox4.GetBindingExpression(TextBox.TextProperty).UpdateTarget();
+			attackbonusbox4.GetBindingExpression(TextBox.TextProperty).UpdateTarget();
+		}
+		
 		void GoldButtonClick(object sender, RoutedEventArgs e)
 		{
 			Button btn = new Button();
@@ -179,6 +245,64 @@ namespace FourAgainstDarkness.Views
 					break;
 			}
 				}
+		
+		void GoldButton3Click(object sender, RoutedEventArgs e)
+		{
+			Button btn = new Button();
+			btn=(Button)sender;
+			string name = btn.Content.ToString();
+			switch(name)
+			{
+				case "+1":
+					_character3.Gold=_character3.Gold+1;
+					goldbox3.GetBindingExpression(TextBox.TextProperty).UpdateTarget();
+					break;
+					
+				case "+10":
+					_character3.Gold=_character3.Gold+10;
+					goldbox3.GetBindingExpression(TextBox.TextProperty).UpdateTarget();
+					break;
+					
+				case "-1":
+					if(_character3.Gold>0) _character3.Gold=_character3.Gold-1;
+					goldbox3.GetBindingExpression(TextBox.TextProperty).UpdateTarget();
+					break;
+					
+				case "-10":
+					if(_character3.Gold>9) _character3.Gold=_character3.Gold-10;
+					goldbox3.GetBindingExpression(TextBox.TextProperty).UpdateTarget();
+					break;
+			}
+				}
+				
+		void GoldButton4Click(object sender, RoutedEventArgs e)
+		{
+			Button btn = new Button();
+			btn=(Button)sender;
+			string name = btn.Content.ToString();
+			switch(name)
+			{
+				case "+1":
+					_character4.Gold=_character4.Gold+1;
+					goldbox4.GetBindingExpression(TextBox.TextProperty).UpdateTarget();
+					break;
+					
+				case "+10":
+					_character4.Gold=_character4.Gold+10;
+					goldbox4.GetBindingExpression(TextBox.TextProperty).UpdateTarget();
+					break;
+					
+				case "-1":
+					if(_character4.Gold>0) _character4.Gold=_character4.Gold-1;
+					goldbox4.GetBindingExpression(TextBox.TextProperty).UpdateTarget();
+					break;
+					
+				case "-10":
+					if(_character4.Gold>9) _character4.Gold=_character4.Gold-10;
+					goldbox4.GetBindingExpression(TextBox.TextProperty).UpdateTarget();
+					break;
+			}
+				}
 				
 		void AddButton(object sender, RoutedEventArgs e)
 		{
@@ -212,6 +336,40 @@ namespace FourAgainstDarkness.Views
 			StackPanel pan = new StackPanel();
 			pan.Orientation=Orientation.Horizontal;
 			spellpanel2.Children.Add(pan);
+			TextBox txt = new TextBox();
+			pan.Children.Add(txt);
+			txt.Width=125;
+			CheckBox chk = new CheckBox();
+			chk.VerticalAlignment=VerticalAlignment.Center;
+			pan.Children.Add(chk);
+			Button btn = new Button();
+			pan.Children.Add(btn);
+			btn.Content="+1";
+			btn.Click+=AddButton;		
+		}
+		
+		void AddRow3(object sender, RoutedEventArgs e)
+		{
+			StackPanel pan = new StackPanel();
+			pan.Orientation=Orientation.Horizontal;
+			spellpanel3.Children.Add(pan);
+			TextBox txt = new TextBox();
+			pan.Children.Add(txt);
+			txt.Width=125;
+			CheckBox chk = new CheckBox();
+			chk.VerticalAlignment=VerticalAlignment.Center;
+			pan.Children.Add(chk);
+			Button btn = new Button();
+			pan.Children.Add(btn);
+			btn.Content="+1";
+			btn.Click+=AddButton;		
+		}
+		
+		void AddRow4(object sender, RoutedEventArgs e)
+		{
+			StackPanel pan = new StackPanel();
+			pan.Orientation=Orientation.Horizontal;
+			spellpanel4.Children.Add(pan);
 			TextBox txt = new TextBox();
 			pan.Children.Add(txt);
 			txt.Width=125;
